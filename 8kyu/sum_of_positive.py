bfa9b83381b9c68e1488a9dec2576c349508fb08
jmbatch <jrdbatch@outlook.com>
2024-08-31 12:28:46 -0500

    added 8kyu kata descriptions

--- 8kyu/sum_of_positive.py
+++ 8kyu/sum_of_positive.py
@@ -1,3 +1,10 @@
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+
+
 def positive_sum(arr):
     neg_num = [num for num in arr if num < 0]
     if len(neg_num) == len(arr):
